1357702f58ead99677831caf2d7688def95f3caa
= <theo.hautois@gmail.com>
2024-10-05 14:28:44 -0400

    modification de vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio/",
+  base:"/Portfolio/",
   plugins: [
     vue(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
 })
