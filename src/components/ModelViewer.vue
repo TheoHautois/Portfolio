ac796b0bd0b8a4cdc5d67241b9c389f8240c88fc
= <theo.hautois@gmail.com>
2024-10-09 20:03:44 -0400

    page /3d - v1

--- src/components/ModelViewer.vue
+++ src/components/ModelViewer.vue
@@ -1,78 +1,131 @@
 <script setup lang = "ts">
-import { defineComponent, onMounted, ref } from 'vue';
+import { defineComponent, onMounted, ref , defineExpose} from 'vue';
 import * as THREE from 'three';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {GLTFLoader, type GLTF} from 'three/examples/jsm/loaders/GLTFLoader.js';
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 
-
-const objPath = '/src/assets/models/tankYou/kv2.glb';
 const canvasContainer = ref<HTMLDivElement | null>(null);
 
+// création de la scene 3D
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+
+// Ajout d'une source de lumiere
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(5, 5, 5);
+scene.add(light);
+
+// lumière ambiante
+const ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
+
+// pour charger les models 3D
+const modelLoader = new GLTFLoader();
+let currentModel: GLTF | null = null;
+let currentModelPath:string = "";
+
+let isLoading = ref(false);
+
 onMounted(() => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
+    //réactualisation la taille du component
     updateSize();
+
+    // On ajoute le renderer au canvasContainer
     canvasContainer.value?.appendChild(renderer.domElement);
 
+    // position de la camera
+    camera.position.z = 5;
 
-    // Ajout d'une source de lumiere
-    const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(5, 5, 5).normalize();
-    scene.add(light);
-    const ambientLight = new THREE.AmbientLight(0x404040); // Lumière ambiante
-    scene.add(ambientLight);
 
-    const loader = new GLTFLoader();
-    loader.load(objPath, (gltf) => {
-        scene.add(gltf.scene);
-        camera.position.z = 5;
 
-        const controls = new OrbitControls(camera, renderer.domElement);
-        controls.enableDamping = true;
-
-        function animate()
+    // controles de la camera :
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    function animate()
         {
             requestAnimationFrame(animate);
             controls.update();
             renderer.render(scene, camera);
         };
         animate();
-    });
-
-    // en cas de resize de la fenêtre
-    window.addEventListener('resize', () => {
-        updateSize();
-    })
-
-    function updateSize()
-    {
-        if (canvasContainer == null) return;
-
-        let width = canvasContainer.value?.clientWidth ? canvasContainer.value.clientWidth : 0;
-        let height = canvasContainer.value?.clientHeight ? canvasContainer.value.clientHeight : 0;
-
-        console.log('X: ' + canvasContainer.value?.clientWidth);
-        console.log('Y: ' + canvasContainer.value?.clientHeight);
-        renderer.setSize(width, height);
-
-        camera.aspect = width / height;
-        camera.updateProjectionMatrix();
-
-
-
-
-    }
 });
+
+// en cas de resize de la fenêtre
+window.addEventListener('resize', () => {
+    updateSize();
+})
+
+function updateSize()
+{
+    console.log('resize !');
+    if (canvasContainer == null) return;
+
+    let width = canvasContainer.value?.clientWidth ? canvasContainer.value.clientWidth : 0;
+    let height = canvasContainer.value?.clientHeight ? canvasContainer.value.clientHeight : 0;
+
+    renderer.setSize(width, height);
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+}
+
+function loadModel(path:string){
+    if (currentModelPath == path) return; // car ça sert à rien de charger un modèle déjà affiché...
+    isLoading.value = true;
+    modelLoader.load(path, (gltf) => {
+        console.log('loading model at :' + path);
+
+        // on enleve le modèle d'avant, et on le remplace par le nouveau.
+        if (currentModel != null)
+            scene.remove(currentModel.scene);
+
+        scene.add(gltf.scene);
+        currentModel = gltf;
+        currentModelPath = path;
+        isLoading.value = false;
+    });
+}
+
+defineExpose({
+    loadModel,
+});
+
+//<div class="loading-screen">Chargement du modèle 3D...</div>
 </script>
 
 <template>
-<div ref="canvasContainer" class="canvas-container"></div>
+
+<div class = container>
+    <div ref="canvasContainer" class="canvas-container"></div>
+    <div v-if="isLoading" class="loading-screen">Chargement du modèle 3D...</div>
+</div>
+
 </template>
 
 <style scoped>
+
+.container {
+    position: relative;
+}
+
 .canvas-container{
+    border:solid 2px red;
     width: 100%;
     height: 100%;
 }
+
+.loading-screen{
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: rgba(0, 0, 0, 0.6);
+}
+
+
 </style>
--- src/views/3DView.vue
+++ src/views/3DView.vue
@@ -1,12 +1,46 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import ModelViewer from '../components/ModelViewer.vue';
+import { bool } from 'three/webgpu';
+
+const modelViewer = ref();
+
+// comme les liens change entre develop et build, j'en avais marre de me casser la tete alors j'ai trouver cette solution.
+// c'est pas de la poésie MAIS CA FONCTIONNE !
+console.log('MODE : ' + import.meta.env.MODE);
+let devMode:boolean = import.meta.env.MODE == 'development';
+let basePath:string;
+
+if (devMode)
+    basePath = '/src';
+else
+    basePath = '/Portfolio';
+
+console.log('basePath : ' + basePath);
+    
+// liste de tous les modèle 3D disponnible.
+const models = {
+    'kv2' :     basePath + '/assets/models/tankYou/kv2.glb',
+    'panzer2' : basePath + '/assets/models/tankYou/panzer2.glb',
+    'maus' :    basePath + '/assets/models/tankYou/maus.glb'
+};
+
+function loadTester(path:string)
+{
+    modelViewer.value.loadModel(path);
+}
 </script>
 
 <template>
     <h2>Modélisation 3D :</h2>
     <p class = "margin">En cours de développement...</p>
 
-    <ModelViewer class = "test" />
+    <ModelViewer ref = "modelViewer" class = "test" />
+
+    <h3>Tank You :</h3>
+    <button @click="loadTester(models['panzer2'])">PANZER</button>
+    <button @click="loadTester(models['kv2'])">KV2</button>
+    <button @click="loadTester(models['maus'])">MAUS</button>
 </template>
 
 <style scoped>
@@ -15,9 +49,16 @@ import ModelViewer from '../components/ModelViewer.vue';
     }
 
     .test{
-
         height: 600px;
         width: 70%;
-        border:solid 3px red;
+    }
+
+    button{
+        padding:5px;
+        margin:5px;
+        border-radius: 5px;
+        min-width: 100px;
+        background-color: black;
+        color: white;
     }
 </style>
